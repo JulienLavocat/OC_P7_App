4d000fbe566ed8158fe5fb2c9f1c7127eede348f
Julien Lavocat <julien.lavocat@gmail.com>
2022-09-21 10:31:11 +0200

    feat: rework avatar editor

--- src/components/modules/Profile/Avatar/StyleOptions.tsx
+++ src/components/modules/Profile/Avatar/StyleOptions.tsx
@@ -5,17 +5,20 @@ export default function StyleOptions({
 	styleOptions,
 	optionsFilter,
 	onChange,
+	className,
 }: {
+	className?: string;
 	styleOptions: Record<string, any>;
 	optionsFilter: (e: string, i: number) => void;
 	onChange: (name: string, option: string) => void;
 }) {
 	return (
-		<div className="w-full flex flex-col items-center">
+		<div className={`w-full flex flex-col items-center ${className}`}>
 			{Object.keys(styleOptions)
 				.filter(optionsFilter)
-				.map((optionName) => (
+				.map((optionName, i) => (
 					<StyleOptionPicker
+						key={optionName + i}
 						name={optionName}
 						options={styleOptions[optionName]}
 						onPick={(e) => onChange(optionName, e)}
--- src/components/modules/Profile/Avatar/index.tsx
+++ src/components/modules/Profile/Avatar/index.tsx
@@ -1,70 +1,102 @@
 import { current } from "@reduxjs/toolkit";
 import React, { useEffect, useState } from "react";
-import { Select } from "react-daisyui";
+import { Button, Select, Tabs } from "react-daisyui";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { useAppSelector } from "../../../../hooks/useAppDispatch";
 import StyleOptionPicker from "./StyleOptionPicker";
 import StyleOptions from "./StyleOptions";
-import styles from "./styles.json";
+import styles from "./styles2.json";
 
 export default function Avatar() {
-	const user = useAppSelector((s) => s.user);
+	const user = useAppSelector((s) => s.user?.user);
 	const [currentAvatar, setCurrentAvatar] = useState("");
-	const [currentStyle, setCurrentStyle] = useState(Object.keys(styles)[0]);
 	const [selectedOptions, setselectedOptions] = useState({});
-
-	const styleOptions = (styles as Record<string, any>)[currentStyle];
+	const [currentCategory, setCurrentCategory] = useState(
+		Object.keys(styles)[0]
+	);
 
 	useEffect(() => {
-		const params = Object.entries(selectedOptions).filter(
+		const params = Object.entries<string>(selectedOptions).filter(
 			(e) => e[1] !== "none"
 		);
 
+		if (params.find((e) => e[0] === "accessories"))
+			params.push(["accessoriesChance", "100"]);
+
+		if (params.find((e) => e[0] === "facialHair"))
+			params.push(["facialHairChance", "100"]);
+
 		setCurrentAvatar(
-			`http://dicebear.julienlavocat.me/api/${currentStyle}/.svg?${new URLSearchParams(
-				params as string[][]
-			).toString()}&accessoiresProbability=${
-				params.find((e) => e[0] === "accessoires") ? 100 : 0
-			}`
+			`http://dicebear.julienlavocat.me/api/avataaars/.svg?${new URLSearchParams(
+				params
+			).toString()}`
 		);
 		return () => {};
-	}, [selectedOptions, currentStyle]);
+	}, [selectedOptions]);
+
+	const onSave = () => {};
 
 	return (
-		<div className="grid grid-cols-3 bg-base-100 rounded-lg shadow">
-			<StyleOptions
-				optionsFilter={(_, i) => i % 2 === 0}
-				styleOptions={styleOptions}
-				onChange={(name, option) =>
-					setselectedOptions((current) => ({
-						...current,
-						[name]: option,
-					}))
-				}
-			/>
-			<div className="flex flex-col h-full justify-around">
-				<img className="w-full" src={currentAvatar || user?.image} />
-				<Select
-					defaultValue={currentStyle}
-					children={Object.keys(styles).map((e) => (
-						<option value={e}>{e}</option>
+		<div className="flex flex-row flex-wrap bg-base-100 rounded-lg shadow justify-center">
+			<div className="flex flex-col items-center h-96 justify-around">
+				<img
+					className="w-full max-w-[20rem]"
+					src={currentAvatar || user?.image}
+				/>
+				<Button className="w-min" onClick={onSave} color="primary">
+					Sauvegarder
+				</Button>
+			</div>
+			<div className="border-l w-full sm:w-max">
+				<Tabs
+					value={currentCategory}
+					onChange={setCurrentCategory}
+					className="border-b h-10 items-center">
+					{Object.keys(styles).map((e) => (
+						<Tabs.Tab value={e} key={e} className="flex-grow">
+							{e}
+						</Tabs.Tab>
 					))}
-					onChange={(e) => {
-						setCurrentStyle(e as string);
-						setselectedOptions({});
-					}}
+				</Tabs>
+				<StyleOptions
+					optionsFilter={() => true}
+					styleOptions={(styles as any)[currentCategory]}
+					onChange={(name, option) =>
+						setselectedOptions((current) => ({
+							...current,
+							[name]: option,
+						}))
+					}
 				/>
 			</div>
-			<StyleOptions
-				optionsFilter={(_, i) => i % 2 !== 0}
-				styleOptions={styleOptions}
-				onChange={(name, option) =>
-					setselectedOptions((current) => ({
-						...current,
-						[name]: option,
-					}))
-				}
-			/>
 		</div>
 	);
+
+	// return (
+	// 	<div className="grid grid-cols-3 bg-base-100 rounded-lg shadow">
+	// 		<StyleOptions
+	// 			optionsFilter={(_, i) => i % 2 === 0}
+	// 			styleOptions={styles}
+	// 			onChange={(name, option) =>
+	// 				setselectedOptions((current) => ({
+	// 					...current,
+	// 					[name]: option,
+	// 				}))
+	// 			}
+	// 		/>
+	// 		<div className="flex flex-col h-full justify-around">
+	// 			<img className="w-full" src={currentAvatar || user?.image} />
+	// 		</div>
+	// 		<StyleOptions
+	// 			optionsFilter={(_, i) => i % 2 !== 0}
+	// 			styleOptions={styles}
+	// 			onChange={(name, option) =>
+	// 				setselectedOptions((current) => ({
+	// 					...current,
+	// 					[name]: option,
+	// 				}))
+	// 			}
+	// 		/>
+	// 	</div>
+	// );
 }
